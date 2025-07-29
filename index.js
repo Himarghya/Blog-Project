6c53e570cea943d6ac1e28d187947a4425b64a16
Himarghya Das <himarghyadas@gmail.com>
2025-07-29 10:39:12 +0530

    Blog Project

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import axios from "axios";
+import bodyParser from 'body-parser';
 
 const port = 3000;
 const app = express();
 const API_URL = "http://localhost:4000";
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(bodyParser.json());
 
 app.get("/", async (req, res) => {
     const response = await axios.get(API_URL + "/all")
@@ -14,8 +17,36 @@ app.get("/", async (req, res) => {
         data: result
     });
 })
+//After pressing New Post
 app.get("/NewPost", (req, res) => {
     res.render("modify.ejs");
+})
+//Submit button for New Post
+app.post("/Submit", async(req, res) =>{
+         const response = await axios.post( `${API_URL}/post`, req.body);
+         res.redirect("/");
+})
+app.get("/EditText/:id", async(req, res) =>{
+    const ID = parseInt(req.params.id);
+    const response = await axios.get(`${API_URL}/all/${ID}`);
+    const data = response.data;
+    res.render("Edit.ejs", {
+        Data : data
+    });
+})
+app.post("/Submit/Edit/:id", async(req, res) =>{
+    const ID = req.params.id;
+    const response = await axios.patch(`${API_URL}/CompleteEdit/${ID}` , req.body);
+    res.redirect("/")
+})
+app.get("/DeleteText/:id" , async(req, res)=>{ 
+    try {
+    const ID = parseInt(req.params.id);
+    await axios.delete(`${API_URL}/Delete/${ID}`);
+    res.redirect("/");  
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting post" });
+  }
 })
 
 app.listen(port, () => {
--- render.js
+++ render.js
@@ -5,6 +5,7 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 4000;
 
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/all", (req, res) => {
@@ -12,29 +13,57 @@ app.get("/all", (req, res) => {
     res.json(Blog);
 })
 
+app.get("/all/:id" , (req, res)=>{
+    const ID = parseInt(req.params.id);
+    const CorrectBlog = blog.find((h) => h.id === ID);
+    res.json(CorrectBlog);
+})
+
 app.post("/post", (req, res) => {
     const post = {
         id: blog.length + 1,
-        Tittle: req.params.Tittle,
-        DayOfCretaion: "",
-        BlogText: req.params.BlogText,
-        Author: req.params.Author
+        Tittle: req.body.Tittle,
+        DayOfCretaion: Date(),
+        BlogText: req.body.Context,
+        Author: req.body.Author
     }
     blog.push(post);
+    console.log(post);
     res.json(blog);
 })
 
-const blog = [{
+app.patch(`/CompleteEdit/:id`, (req, res) => {
+    const ID = parseInt(req.params.id);
+    const ArryNo = blog.find((N)=> N.id === ID);
+    const post = {
+        id :ID,
+        Tittle: req.body.Tittle || ArryNo.Tittle,
+        BlogText: req.body.Context || ArryNo.BlogText,
+        Author: req.body.Author || ArryNo.Author
+    }
+    const IDno = blog.findIndex((N)=> N.id === ID);
+    blog[IDno] = post;
+    res.json(post);
+})
+
+app.delete(`/Delete/:id` , (req, res)=>{
+    const ID = parseInt(req.params.id);
+    const IndexNo = blog.findIndex((Index) => Index.id === ID);
+    blog.splice(IndexNo , 1);
+    res.json();
+})
+
+var blog = [{
     id: 1,
     Tittle: "The Rise of Decentralised Finance",
-    DayOfCretaion: "2023-08-01 T 10:00:00",
+    DayOfCretaion: "Wed June 28 2025 12:00:19 GMT+08:30 (India Standard Time)",
     BlogText: "Decentralized Finance (DeFi) is an emerging and rapidly evolving field in the blockchain industry. It refers to the shift from traditional, centralized financial systems to peer-to-peer finance enabeled by decentralized technologies built on Ethereum and other blockchains. With the promise of reduced dependency on the traditional banking sector, DeFi platforms offer a wide range of services, from lending and borrowing to insurance and trading ",
     Author: "Alex Thompson"
 },
 {
     id: 2,
     Tittle: "The Impact of Artificial Intelligence on Modern Business",
-    DayOfCretaion: "2023-08-05 T 14:30:00",
+    DayOfCretaion: "Tue Aug 08 2024 10:00:00 GMT+10:30 (India Standard Time)",
     BlogText: "Artificial Intelligence (AI) is no logr a concept of future. It's very much a part of our present, reshaping industries and enhancing the capibilities of existing systems.From automatinng routine tasks to offering intelligent insights, AI is proving to be boon for businesses. With advancements in machine learning and deep learning, businesses can now address previously insurmountable and tap inti new oppurtunities",
     Author: "Mia Williams"
 }]
